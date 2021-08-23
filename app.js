fb45e6487fc09e02e06acb70cec75b09c91535c6
Ali Rıza Oyan <aliriza@hotelrunner.com>
2021-08-23 17:43:29 +0300

    countdown feature added

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ const timeLeft = document.querySelector("#time-left")
 let score = document.querySelector("#score")
 
 let result = 0
-let currentTime = timeLeft.textContent
+let currentTime = parseInt(timeLeft.textContent)
+let countDownID
+let timerID
+
+let checkMoleLocation = (event) => {
+    if (event.target.id === hitElement) {
+        result++
+        score.textContent = result
+    }
+}
 
 let moveMole = () => {
     square.forEach(element => {
@@ -17,18 +26,28 @@ let moveMole = () => {
 }
 
 square.forEach((element) => {
-    element.addEventListener("mouseup", () => {
-        if (element.id === hitElement) {
-            result++
-            score.textContent = result
-        }
-    })
+    element.addEventListener("mouseup", checkMoleLocation)
 })
 
+let countDown = () => {
+    currentTime--
+    timeLeft.textContent = String(currentTime)
+
+    if (currentTime === 0) {
+        square.forEach((element) => {
+            element.removeEventListener("mouseup", checkMoleLocation)
+        })
+
+        clearInterval(timerID)
+        clearInterval(countDownID)
+
+        alert("Time's up! Your score is " + result)
+    }
+}
+
 let startGame = () => {
-    let timerID = null
     timerID = setInterval(moveMole, 1000)
+    countDownID = setInterval(countDown, 1000)
 }
 
 startGame()
-
